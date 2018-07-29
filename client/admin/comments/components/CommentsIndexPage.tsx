3935901e49d37cbb73dc60d781e67b7bebd7c451
Radim <radim.janos@seznam.cz>
2018-07-29 22:31:43 +0200

    State working (initial and read), paper elevation and padding.

--- client/admin/comments/components/CommentsIndexPage.tsx
+++ client/admin/comments/components/CommentsIndexPage.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import {WithAdminProps} from '@client/with/withAdmin';
+import {Button, Paper} from '@material-ui/core';
+import {RemoveCircle} from '@material-ui/icons';
 
-export class CommentsIndexPage extends React.Component<WithAdminProps> {
+interface State {
+    messages: any[];
+    message: string;
+}
+
+export class CommentsIndexPage extends React.Component<WithAdminProps, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            messages: [],
+            message: 'Testing text',
+        };
+    }
+
     render() {
-        return <div>CommentsIndexPage</div>;
+        return (
+            <div>
+                Radim's CommentsIndexPage
+                <Button variant="raised">Push me</Button>
+                <RemoveCircle />
+                <Paper elevation={5}>
+                    <p style={{padding: '20px'}}>{this.state.message}</p>
+                </Paper>
+            </div>
+        );
     }
 }
