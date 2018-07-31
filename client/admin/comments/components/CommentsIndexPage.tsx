2257ebf89208152cd7cf3ab7247fd88328a52fb5
Radim <radim.janos@seznam.cz>
2018-07-31 22:58:15 +0200

    Added interface props; Created deletion dialog; Improved del icon.

--- client/admin/comments/components/CommentsIndexPage.tsx
+++ client/admin/comments/components/CommentsIndexPage.tsx
@@ -1,32 +1,49 @@
-import * as React from 'react';
 import {WithAdminProps} from '@client/with/withAdmin';
-import {Button, Paper, ClickAwayListener, TextField} from '@material-ui/core';
+import {Button, ClickAwayListener, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, Paper, TextField} from '@material-ui/core';
 import {RemoveCircle} from '@material-ui/icons';
+import * as React from 'react';
+
+type CommentModel = string;
 
 interface State {
-    comments: any[];
-    comment: string;
-    open: boolean;
+    comments: CommentModel[];
+    comment: CommentModel;
+    openAdd: boolean;
+    openAlert: boolean;
+    alertIndex: number;
 }
 
-const Messages = (
-    props: any,
-): any => // why does "props: any[]" not work here?
+interface MessagesProps {
+    comments: CommentModel[];
+    openAlert: (index: number) => () => void;
+}
+
+const Messages = (props: MessagesProps): any =>
     props.comments.map((text: any, index: number) => (
         <div style={{padding: '15px 10%'}} key={index}>
             <Paper elevation={5}>
-                <div style={{padding: '20px 20px 0px 20px'}}>{text}</div>
-                <div style={{textAlign: 'right'}}>
-                    <RemoveCircle onClick={props.dellComment(index)} />
+                <div style={{padding: '20px 20px 20px 20px'}}>
+                    {text}
+                    <div style={{float: 'right'}}>
+                        <IconButton onClick={props.openAlert(index)} aria-label="Delete">
+                            <RemoveCircle />
+                        </IconButton>
+                    </div>
                 </div>
             </Paper>
         </div>
     ));
 
-const AddComment = (props: any): any => (
+interface AddCommentProps {
+    comment: CommentModel;
+    handleChange: () => void;
+    addComment: () => void;
+}
+
+const AddComment = (props: AddCommentProps): any => (
     <Paper>
         <form style={{padding: '20px 30px'}} noValidate autoComplete="off">
-            <TextField name="comment" multiline fullWidth value={props.comment} onChange={props.handleChange} margin="normal" />
+            <TextField name="comment" label="Comment here" autoFocus multiline fullWidth value={props.comment} onChange={props.handleChange} margin="normal" />
             <div style={{paddingTop: '20px'}}>
                 <Button onClick={props.addComment} variant="raised">
                     Post
@@ -36,25 +53,63 @@ const AddComment = (props: any): any => (
     </Paper>
 );
 
+interface DelAlertProps {
+    openAlert: boolean;
+    closeAlert: (index?: number) => () => void;
+    alertIndex: number;
+}
+const DelAlert = (props: DelAlertProps): JSX.Element => (
+    <Dialog open={props.openAlert} onClose={props.closeAlert()}>
+        <DialogTitle>{'Are you sure you want to permanently delete this comment?'}</DialogTitle>
+        <DialogContent>
+            <DialogContentText>If you delete this comment, it will be permanently lost. What a shame...</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={props.closeAlert()}>Cancel</Button>
+            <Button onClick={props.closeAlert(props.alertIndex)} autoFocus>
+                Delete
+            </Button>
+        </DialogActions>
+    </Dialog>
+);
+
 export class CommentsIndexPage extends React.Component<WithAdminProps, State> {
     constructor(props: any) {
         super(props);
         this.state = {
             comments: ['this is a crazy test', 'Testing text', 'hello hello', 'how are you'],
             comment: '',
-            open: false,
+            openAdd: false,
+            openAlert: false,
+            alertIndex: 0,
         };
     }
 
     handleClick = () => {
         this.setState({
-            open: !this.state.open,
+            openAdd: !this.state.openAdd,
         });
     };
 
     handleClickAway = () => {
         this.setState({
-            open: false,
+            openAdd: false,
+        });
+    };
+
+    openAlert = (index: number) => () => {
+        this.setState({
+            openAlert: true,
+            alertIndex: index,
+        });
+    };
+
+    closeAlert = (index?: number) => () => {
+        if (index !== undefined) {
+            this.handleDel(index);
+        }
+        this.setState({
+            openAlert: false,
         });
     };
 
@@ -67,7 +122,7 @@ export class CommentsIndexPage extends React.Component<WithAdminProps, State> {
     // Couldn't get this to work couse of TS :(
     // handleChange = (name: string): any => (event: any): void => {
     //     this.setState({
-    //       [name]: event.target.value
+    //       [name]: event.target.value     // something is wrong here
     //     });
     // }
 
@@ -77,11 +132,11 @@ export class CommentsIndexPage extends React.Component<WithAdminProps, State> {
         this.setState({
             comments,
             comment: '',
-            open: false,
+            openAdd: false,
         });
     };
 
-    dellComment = (index: number) => (): void => {
+    handleDel = (index: number): void => {
         const {comments} = this.state;
         comments.splice(index, 1);
         this.setState({
@@ -97,14 +152,15 @@ export class CommentsIndexPage extends React.Component<WithAdminProps, State> {
                         <Button onClick={this.handleClick} variant="raised">
                             Add comment
                         </Button>
-                        {this.state.open ? (
+                        {this.state.openAdd ? (
                             <div style={{marginTop: '30px'}}>
                                 <AddComment comment={this.state.comment} handleChange={this.handleChange} addComment={this.addComment} />
                             </div>
                         ) : null}
                     </div>
                 </ClickAwayListener>
-                <Messages comments={this.state.comments} dellComment={this.dellComment} />
+                <Messages comments={this.state.comments} openAlert={this.openAlert} />
+                <DelAlert openAlert={this.state.openAlert} closeAlert={this.closeAlert} alertIndex={this.state.alertIndex} />
             </div>
         );
     }
