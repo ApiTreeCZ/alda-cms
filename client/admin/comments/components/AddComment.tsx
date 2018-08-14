c1822ce6c6ac64f900dc3a01834d47bd6d5376bd
Radim <radim.janos@seznam.cz>
2018-08-14 13:31:15 +0900

    AddComment - withLoading HOC

--- client/admin/comments/components/AddComment.tsx
+++ client/admin/comments/components/AddComment.tsx
@@ -1,12 +1,13 @@
-import {Button, Paper, TextField} from '@material-ui/core';
+import {Button, Paper, TextField, LinearProgress} from '@material-ui/core';
+import React from 'react';
 
 interface AddCommentProps {
     message: string;
-    handleChange: () => void;
+    handleChange: (event: any) => void;
     addComment: () => void;
 }
 
-export const AddComment = (props: AddCommentProps): any => (
+const Component = (props: AddCommentProps): any => (
     <Paper style={{margin: '30px 25%'}}>
         <form style={{padding: '20px 30px 30px 30px'}} noValidate autoComplete="off">
             <TextField name="message" label="Comment here" autoFocus multiline fullWidth value={props.message} onChange={props.handleChange} margin="normal" />
@@ -16,3 +17,34 @@ export const AddComment = (props: AddCommentProps): any => (
         </form>
     </Paper>
 );
+
+const withLoading = (BaseComponent: React.ComponentType<AddCommentProps>): React.ComponentClass<AddCommentProps> => {
+    const initialState = {
+        isLoading: false,
+    };
+
+    return class extends React.Component<AddCommentProps, typeof initialState> {
+        readonly state = initialState;
+
+        handleAddComment = () => {
+            this.setState({isLoading: true});
+            setTimeout(() => {
+                this.props.addComment();
+                this.setState({isLoading: false});
+            }, 1000);
+        };
+
+        render() {
+            const {addComment, ...props} = this.props;
+            const {isLoading} = this.state;
+            return (
+                <div>
+                    <BaseComponent addComment={this.handleAddComment} {...props} />
+                    {isLoading && <LinearProgress />}
+                </div>
+            );
+        }
+    };
+};
+
+export const AddComment = withLoading(Component);
